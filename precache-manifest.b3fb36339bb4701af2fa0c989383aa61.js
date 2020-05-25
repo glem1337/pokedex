d3312e4d9374e05693b9d40864f4e246e0c73279
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-05-25 17:13:32 +0300

    Updates

---
+++ precache-manifest.b3fb36339bb4701af2fa0c989383aa61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6556ab9181e9fbedfaecceeb29bc247a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "4407a43f89f2554bf374",
+    "url": "/pokedex/static/js/2.28484aa8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.28484aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6aaea7ac603b736158",
+    "url": "/pokedex/static/js/main.43f81997.chunk.js"
+  },
+  {
+    "revision": "515fe8b9326c02e3f4ff",
+    "url": "/pokedex/static/js/runtime-main.8dbfc64c.js"
+  },
+  {
+    "revision": "3726ed66e6b743fa8394125b334b231c",
+    "url": "/pokedex/static/media/logo.3726ed66.svg"
+  }
+]);
